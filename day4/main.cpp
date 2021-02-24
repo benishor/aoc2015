c68737bf5434d40650d47be2123f5b657aab6cfc
Adrian Scripca <benishor@gmail.com>
2021-02-24 13:29:07 +0200

    Day 4: complete

--- day4/main.cpp
+++ day4/main.cpp
@@ -1,5 +1,49 @@
 #include "solution.h"
+#include "md5.h"
 
-int main() {
-    return 0;
+class day4 : public aoc::solution {
+protected:
+    void run(std::istream& in, std::ostream& out) override {
+        std::string secret;
+        std::getline(in, secret);
+
+        auto part1{0UL}, part2{0UL};
+        unsigned long key{1};
+        std::stringstream hash_input;
+
+        while (true) {
+            hash_input.str("");
+            hash_input << secret << key;
+
+            auto md5 = MD5(hash_input.str());
+            auto digest = md5.hexdigest();
+
+            if (digest.substr(0, 5) == "00000") {
+                if (part1 == 0UL) {
+                    part1 = key;
+                }
+                if (digest.at(5) == '0') {
+                    part2 = key;
+                    break;
+                }
+            }
+            key++;
+        }
+
+        out << part1 << std::endl;
+        out << part2 << std::endl;
+    }
+
+    void register_tests() override {
+        add_test(aoc::test{
+                .name = "First",
+                .input = "abcdef",
+                .expected_output = "609043\n6742839"
+        });
+    }
+};
+
+int main(int argc, char** argv) {
+    day4 solution{};
+    return solution.execute(argc, argv);
 }
