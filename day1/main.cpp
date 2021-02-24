84cf4f046ecf6b371754e158c43f92396432614b
Adrian Scripca <benishor@gmail.com>
2021-02-24 11:30:40 +0200

    Day 1: complete

--- day1/main.cpp
+++ day1/main.cpp
@@ -1,5 +1,46 @@
 #include "solution.h"
 
-int main() {
-    return 0;
+class day1 : public aoc::solution {
+protected:
+    void run(std::istream& in, std::ostream& out) override {
+        int floor{0};
+        int position{1};
+        int basement_position{0};
+        for (char c; in >> c; position++) {
+            switch (c) {
+                case '(':
+                    floor++;
+                    break;
+                case ')':
+                    floor--;
+                    break;
+                default:
+                    break;
+            }
+            if (floor == -1 && basement_position == 0) {
+                basement_position = position;
+            }
+        }
+        out << floor << std::endl;
+        out << basement_position << std::endl;
+    }
+
+    void register_tests() override {
+        add_test(aoc::test{
+                .name = "First",
+                .input = ")",
+                .expected_output = "-1\n1"
+        });
+        add_test(aoc::test{
+                .name = "Second",
+                .input = ")())())",
+                .expected_output = "-3\n5"
+        });
+    }
+
+};
+
+int main(int argc, char** argv) {
+    day1 solution{};
+    return solution.execute(argc, argv);
 }
