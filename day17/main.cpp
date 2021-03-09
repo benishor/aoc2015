c374366919c756bbb4874ee66e3dde794994b8c9
Adrian Scripca <benishor@gmail.com>
2021-03-09 13:41:04 +0200

    Day 17: complete

--- day17/main.cpp
+++ day17/main.cpp
@@ -1,5 +1,43 @@
+#include <numeric>
 #include "solution.h"
 
-int main() {
-    return 0;
+class day17 : public aoc::solution {
+protected:
+
+    void run(std::istream& in, std::ostream& out) override {
+        auto volumes = aoc::elements_from_stream<int>(in);
+        std::sort(volumes.rbegin(), volumes.rend());
+
+        std::map<int, int> stats{};
+        for (size_t i = 0; i < volumes.size(); i++) {
+            count_possibilities(volumes, i, 150, stats);
+        }
+
+        size_t part1 = std::accumulate(std::begin(stats), std::end(stats), 0,
+                                       [](int value, const auto& p) {
+                                           return value + p.second;
+                                       }
+        );
+        int part2 = stats.begin()->second;
+
+        out << part1 << std::endl;
+        out << part2 << std::endl;
+    }
+
+    void count_possibilities(std::vector<int>& volumes, size_t index, int remaining, std::map<int, int>& stats,
+                             int depth = 0) {
+        if (volumes[index] == remaining) {
+            stats[depth]++;
+            return;
+        }
+        if (volumes[index] > remaining) return;
+        for (size_t i = index + 1; i < volumes.size(); i++) {
+            count_possibilities(volumes, i, remaining - volumes[index], stats, depth + 1);
+        }
+    }
+};
+
+int main(int argc, char** argv) {
+    day17 solution;
+    return solution.execute(argc, argv);
 }
