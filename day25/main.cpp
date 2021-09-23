e4442e8e0697e5ee8c6cc69e9c200bc69f46f9f2
Adrian Scripca <benishor@gmail.com>
2021-09-23 22:21:39 +0300

    Day 25: complete

--- day25/main.cpp
+++ day25/main.cpp
@@ -1,5 +1,35 @@
 #include "solution.h"
 
-int main() {
-    return 0;
+class day25 : public aoc::solution {
+protected:
+
+    static uint64_t code_for_row_and_col(uint64_t desired_row, uint64_t desired_col) {
+        uint64_t row{1};
+        uint64_t col{1};
+        uint64_t depth{1};
+        uint64_t code{20151125};
+
+        while (row < desired_row || col < desired_col) {
+            if (row > 1) {
+                col++;
+                row--;
+            } else {
+                depth++;
+                row = depth;
+                col = 1;
+            }
+            code = (code * 252533ULL) % 33554393ULL;
+        }
+
+        return code;
+    }
+
+    void run(std::istream& in, std::ostream& out) override {
+        out << code_for_row_and_col(2947, 3029) << std::endl;
+    }
+};
+
+int main(int argc, char **argv) {
+    day25 solution{};
+    return solution.execute(argc, argv);
 }
